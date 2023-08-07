d62e161d6441dcdedf6b3e7c0682f905c6f8a95b
Karol <kpulawski13@gmail.com>
2023-08-07 18:58:55 +0100

    fibonacci iteration

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function fibs(num = 0) {
-  const num1 = 0;
-  const num2 = 1;
-  let result = [num1, num2];
+  let result = [0, 1];
   if (num <= 0) return [];
-  if (num === 1) return [num1];
+  if (num === 1) return result[0];
   for (let index = 2; index < num; index++) {
     result.push(result[result.length - 1] + result[result.length - 2]);
   }
